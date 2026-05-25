52509cab3b2c10773e557b1d770e1c993b5a47ce
Bayu Yunianto <bayuyunianto@Airos-MacBook-Pro.local>
2026-05-25 11:48:50 +0700

    add comment function

--- todo/store.go
+++ todo/store.go
@@ -57,6 +57,7 @@ func (s *Store) Add(title string) (*Todo, error) {
 		return nil, err
 	}
 	// add comment function add
+	// add comment function add 2
 
 	list := NewList(items)
 	todo := list.Add(title)
